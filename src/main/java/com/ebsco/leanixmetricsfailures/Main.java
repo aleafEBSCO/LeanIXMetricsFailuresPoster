8fff691d8af8eeb0f7bd67931f154b2624d8df67
Andrew Leaf <aleaf@ebsco.com>
2018-07-24 16:37:17 -0400

    Split main function into it's own file

---
+++ src/main/java/com/ebsco/leanixmetricsfailures/Main.java
@@ -0,0 +1,64 @@
+package com.ebsco.leanixmetricsfailures;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	//Main function
+    public static void main (String[] args) {
+    	
+    	String apiToken = "";
+    	String workspaceID = "";
+    	
+    	//create a new LeanixMetrics Object
+    	LeanixMetricFailures lm = new LeanixMetricFailures(apiToken, workspaceID);
+    	
+    	//the process can take a few seconds so let the user know it's starting
+    	System.out.println("Starting...");
+    	
+    	/* Failure Types
+    	 * relation
+    	 * Accountable
+    	 * Responsible
+    	 * Business Criticality
+    	 * Owner Persona
+    	 * Functional Fit
+    	 * Technical Fit
+    	 * Quality Seal
+    	 * Model Status
+    	 * Score
+    	 * Documents
+    	 * Lifecycle
+    	 * Business Value Risk
+    	 */
+    	
+    	FilterTools ft = lm.LoadFilterFactsheets();
+    	
+    	if (ft == null) {
+    		return;
+    	}
+    	
+    	System.out.println("Info Loaded");
+    	
+    	Map<String, Integer> metrics = new HashMap<String, Integer>();
+    	metrics.put("relation", ft.getRelationSize());
+    	metrics.put("accountable", ft.getAccountableSize());
+    	metrics.put("responsible", ft.getResponsibleSize());
+    	metrics.put("businessCriticality", ft.getBusinessCriticalitySize());
+    	metrics.put("ownerPersona", ft.getOwnerPersonaSize());
+    	metrics.put("functionalFit", ft.getFunctionalFitSize());
+    	metrics.put("technicalFit", ft.getTechnicalFitSize());
+    	metrics.put("qualitySeal", ft.getQualitySealSize());
+    	metrics.put("modelStatus", ft.getModelStatusSize());
+    	metrics.put("score", ft.getScoreSize());
+    	metrics.put("documents", ft.getDocumentsSize());
+    	metrics.put("lifecycle", ft.getLifecycleSize());
+    	metrics.put("businessValueRisk", ft.getBusinessValueRiskSize());
+    	
+    	System.out.println(metrics.toString());
+
+    	
+    	//push the number of failures
+    	lm.pushPoint(metrics);
+    }
+}
